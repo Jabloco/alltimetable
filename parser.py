2a414949c59f0b06c81df34dc91dc1e11d645eb8
jabloco <zefir4eg@gmail.com>
2022-08-20 21:26:29 +0300

    доделал до 13 столбца

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime
+from types import NoneType
 
 import openpyxl
 
@@ -59,6 +61,81 @@ def contacts_parser(file_link: str) -> list:
     return shops_info_list
 
 
+def alltimetable_parser() -> list:
+    alltimetable_book = openpyxl.load_workbook("alltime.xlsx")
+    worksheet = alltimetable_book.active
+    shops_info_list = []
+    for row in range(1, worksheet.max_row):
+        shop_info = {}
+        shop_info["main_info"] = {}  # словарь с основными данными о ммагазине
+        shop_info["fiscal"] = {}  # словарь с данными о фискальном регистраторе
+        for col in worksheet.iter_cols(2, worksheet.max_column):
+            
+            match col[row].column:               
+                case 2:  # номер магазина
+                    num = col[row].value  # проверяем есть ли данные в ячейке
+                    if num:
+                        shop_info["main_info"]["num_shop"] = re.search('[\d]+', num).group()
+                    else:
+                        shop_info["main_info"]["num_shop"] = None
+                case 3:  # адрес магазина
+                    shop_info["main_info"]["address"] = col[row].value
+                case 4:  # статус магазина
+                    shop_info["main_info"]["status"] = col[row].value
+                case 5:  # юридическое лицо
+                    shop_info["main_info"]["entity"] = col[row].value
+                case 6:  # почтовый индекс
+                    post_index = col[row].value
+                    try:
+                        shop_info["main_info"]["post_index"] = int(post_index)
+                    except TypeError:
+                        shop_info["main_info"]["post_index"] = None
+                case 7:  # КПП
+                    kpp = col[row].value
+                    try:
+                        shop_info["main_info"]["kpp"] = int(kpp)
+                    except TypeError:
+                        shop_info["main_info"]["kpp"] = None
+                case 9:  # модель фискальника
+                    fiscal_model = col[row].value
+                    shop_info["fiscal"]["fiscal_model"] = fiscal_model
+                case 10:  # имя в Такскоме
+                    taxcom_name = col[row].value
+                    shop_info["fiscal"]["taxcom_name"] = taxcom_name
+                case 11:  # заводской номер ккт
+                    fabric_num = col[row].value
+                    try:
+                        shop_info["fiscal"]["fabric_num"] = int(fabric_num)
+                    except TypeError:
+                        shop_info["fiscal"]["fabric_num"] = None
+                case 12:  # регистрационный номер ккт
+                    reg_num = col[row].value
+                    try:
+                        shop_info["fiscal"]["reg_num"] = int(reg_num)
+                    except TypeError:
+                        shop_info["fiscal"]["reg_num"] = None
+                case 13:  # оплата в такскоме до
+                    # taxcom_paid_up_raw = datetime(col[row].value)
+                    # # date = re.match('[\d+\/d+\/d+]',taxcom_paid_up_raw).group(0)
+                    # # try:
+                    # #     str_to_date = datetime.strptime(date, '%m/%d/%Y')
+                    # # except TypeError:
+                    # #     shop_info["fiscal"]["taxcom_paid_up"] = None
+                    # #     continue
+                    # try:
+                    #     taxcom_paid_up = taxcom_paid_up_raw.strftime('%d.%m.%Y')
+                    # except TypeError:
+                    #     taxcom_paid_up = None
+                    # # taxcom_paid_up = str_to_date.strftime('%d.%m.%Y')
+                    # shop_info["fiscal"]["taxcom_paid_up"] = taxcom_paid_up
+                    pass
+
+        shops_info_list.append(shop_info)
+    return shops_info_list
+
+
 if __name__ == "__main__":
     for elem in contacts_parser(contacts_file):
         print(elem)
+    for e in alltimetable_parser():
+        print(e, sep="\n")
