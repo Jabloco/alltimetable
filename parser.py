0b7311f35059c0fa671e0aff968ce6b9787301e4
jabloco <zefir4eg@gmail.com>
2022-09-06 21:34:04 +0300

    попробовал модели pydantic

--- parser.py
+++ parser.py
@@ -2,13 +2,30 @@ import re
 from datetime import datetime
 
 import openpyxl
+from pydantic import BaseModel
+from pydantic import ValidationError
 
 from files_names import contacts_file
 from files_names import monitoring_file
 from files_names import alltime_file
 
 
-def separate_num(words_list: list) -> tuple[dict, dict]:
+class PhoneNum(BaseModel):
+    person_num: list[int] | None = None
+    corp_num: list[int] | None = None
+
+
+class ShopInfo(BaseModel):
+    shop_num: int | None = None
+    num_shipment: list[int] | None = None
+    phone_num: PhoneNum | None
+
+
+class AllShopInfo(BaseModel):
+    all_shop_info: list[ShopInfo]
+
+
+def separate_num(words_list: list) -> tuple[list, list]:
     """
     Функция для разделения корпоративных и личных номеров телефона.
 
@@ -71,7 +88,11 @@ def shop_num(cell_data) -> int | None:
 
 def is_kkt_num(cell_data) -> str | None:
     """
-    Функция для определения являются ли данные в ячейке рег номером ККТ
+    Функция для определения являются ли данные в ячейке каким либо номером кассы
+    
+    Заводской номер и рег.номер ККТ имеют одинаковую длину.
+
+    Что это будет за номер зависит только от столбца
     """
     try:
         return re.match('[\d]{16}', cell_data).group(0)
@@ -81,47 +102,40 @@ def is_kkt_num(cell_data) -> str | None:
         return None
 
 
-def kkt_fabric_num(cell_data) -> str | None:
-    """
-    Функция проверяет являются ли данные в ячейке заводским номером
-    """
-    try:
-        return re.match('[\d]{16}', cell_data).group(0)
-    except TypeError:
-        return None
-    except AttributeError:
-        return None
-
-
-def contacts_parser(file_link: str) -> list:
+def contacts_parser(file_link: str) -> AllShopInfo:
     """
     Из файла парсим номер магазина, номер партии, телефоны
     Возвращает список словарей
     """
     contacts_book = openpyxl.load_workbook(file_link)
     worksheet = contacts_book.active
-    shops_info_list = []
+    all_shop = []
     for row in range(1, worksheet.max_row):
-        shop_info = {}
         for col in worksheet.iter_cols(2, worksheet.max_column):
             match col[row].column:
                 case 2:  # номер маршрута
-                    num_shipment = re.findall('[0-9]+', str(col[row].value))
-                    # преобразуем список строк в список int
-                    num_shipment = [int(item) for item in num_shipment]
-                    shop_info["num_shipment"] = num_shipment
+                    num_shipment_raw = re.findall('[0-9]+', str(col[row].value))
                 case 3:  # номер магазина
-                    shop_info["num_shop"] = int(col[row].value)
+                    shop_num_raw = col[row].value
                 case 6:  # номера телефонов
                     if col[row].value:  # если ячейка не пустая
                         # разбираем ячейку на "слова", формируем список
                         words_list = re.findall('[\w]+', col[row].value)
                     person_phone, corp_phone = separate_num(words_list)
-                    shop_info["phone_num"] = {}
-                    shop_info["phone_num"]["person_num"] = person_phone
-                    shop_info["phone_num"]["corp_num"] = corp_phone
-        shops_info_list.append(shop_info)
-    return shops_info_list
+                    try:
+                        phones = PhoneNum(person_num=person_phone, corp_num=corp_phone)
+                    except ValidationError as e:
+                        print(e.json())
+        try:
+            shop = ShopInfo(
+                num_shipment=num_shipment_raw,
+                shop_num=shop_num_raw,
+                phone_num=phones
+            )
+        except ValidationError as e:
+            print(e.json())
+        all_shop.append(shop)
+    return AllShopInfo(all_shop_info=all_shop)
 
 
 def alltimetable_parser(file_link) -> list:
@@ -264,9 +278,10 @@ def monitoring_parser(file_link: str) -> list:
 
 
 if __name__ == "__main__":
+    print(contacts_parser(contacts_file).json())
     # for elem in contacts_parser(contacts_file):
     #     print(elem)
     # for e in alltimetable_parser(alltime_file):
     #     print(e, sep="\n")
-    for el in monitoring_parser(monitoring_file):
-        print(el, sep="\n")
+    # for el in monitoring_parser(monitoring_file):
+    #     print(el, sep="\n")
