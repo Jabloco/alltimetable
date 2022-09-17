5cb8cdc40610c48c3ea4565375019b462c49d906
jabloco <zefir4eg@gmail.com>
2022-09-17 21:00:57 +0300

    добавил модели таблиц для базы. пока без внешних связей

--- models.py
+++ models.py
@@ -1,15 +1,41 @@
-import typing
-
-from typing import Optional
+from datetime import datetime
 
 from sqlmodel import Field, SQLModel
 
 
-class MainInfo(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    shop_num: int
-    shop_address: Optional[str] = None
-    shop_status: Optional[bool] = None
-    shop_entity: Optional[str] = None
-    shop_kpp: Optional[int] = None
-    cigarettes: bool
+
+class ShopInfo(SQLModel, table=True):
+    id: int | Field(default=None, primary_key=True)
+    shop_num: int | None = None
+    shop_address: str | None = None
+    shop_status: bool | None = None
+    shop_entity: str | None = None
+    shop_kpp: int | None = None
+    cigarettes: bool | None
+
+
+class EntityInfo(SQLModel, table=True):
+    id: int | Field(default=None, primary_key=True)
+    entity_name: str | None = None
+    entity_inn: str | None = None
+
+class ArmInfo(SQLModel, table=True):
+    id: int | Field(default=None, primary_key=True)
+    shop_num: int | None = None
+    arm_comp: str | None = None
+    arm_os: str | None = None
+    arm_shtrih_ver: str | None = None
+    arm_pos_num: int | None = None
+    arm_permit: bool | None = None
+
+class FiscalInfo(SQLModel, table=True):
+    id: int | Field(default=None, primary_key=True)
+    shop_num: int | None = None
+    fiscal_model: str | None = None
+    fiscal_fabric_num: str | None = None
+    fiscal_reg_num: str | None = None
+    fascal_taxcom_name: str | None = None
+    fiscal_taxcom_end_date: datetime | None = None
+    fiscal_fn_num: str | None = None
+    fiscal_fn_period: int | None = None
+    fiscal_fn_end_day: datetime | None = None
