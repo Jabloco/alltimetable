948b1c9f22573a2ea52b9cb2350864660e857c62
jabloco <zefir4eg@gmail.com>
2022-10-21 21:34:52 +0300

    добавил отношения между таблицами

--- models.py
+++ models.py
@@ -1,6 +1,14 @@
 from datetime import datetime
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
+
+
+class EntityInfo(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    entity_name: str | None = None
+    entity_inn: str | None = None
+    """отношение с таблицей магазинов"""
+    shops: list["ShopInfo"] = Relationship(back_populates="entityinfo")
 
 
 class ShopInfo(SQLModel, table=True):
@@ -11,12 +19,13 @@ class ShopInfo(SQLModel, table=True):
     shop_entity: str | None = None
     shop_kpp: int | None = None
     cigarettes: bool | None
-
-
-class EntityInfo(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    entity_name: str | None = None
-    entity_inn: str | None = None
+    """отношение с таблицей юрлиц"""    
+    entity_id: int = Field(default=None, foreign_key="entityinfo.id")
+    entity: EntityInfo = Relationship(back_populates="shopinfo")
+    """отношение с таблицей АРМ"""
+    arm: list["ArmInfo"] = Relationship(back_populates="shopinfo")
+    """отношение с таблицей фискальников"""
+    fiscal: list["FiscalInfo"] = Relationship(back_populates="shopinfo")
 
 
 class ArmInfo(SQLModel, table=True):
@@ -27,6 +36,9 @@ class ArmInfo(SQLModel, table=True):
     arm_shtrih_ver: str | None = None
     arm_pos_num: int | None = None
     arm_permit: bool | None = None
+    """отношение с таблицей магазинов"""
+    shop_id: int = Field(default=None, foreign_key="shopinfo.id")
+    shop: ShopInfo = Relationship(back_populates="arminfo")
 
 
 class FiscalInfo(SQLModel, table=True):
@@ -40,3 +52,6 @@ class FiscalInfo(SQLModel, table=True):
     fiscal_fn_num: str | None = None
     fiscal_fn_period: int | None = None
     fiscal_fn_end_day: datetime | None = None
+    """отнощение с таблицей магазинов"""
+    shop_id: int = Field(default=None, foreign_key="shopinfo.id")
+    shop: ShopInfo = Relationship(back_populates="fiscalinfo")
